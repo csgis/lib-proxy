1f5f2f6c08a6fae3cf9d7fb7e4566f4b5de0a0c8
Víctor González <victor.gonzalez@geomati.co>
2016-06-09 12:50:09 +0200

    Refactor the proxy plugin so it can be used with different URLs, secured for logged users (by URL) and providing basic auth for destination URL. Refs #3030

---
+++ src/main/java/de/csgis/geobricks/proxy/ConfigurableProxyServlet.java
@@ -0,0 +1,39 @@
+package de.csgis.geobricks.proxy;
+
+import java.io.IOException;
+import java.net.URI;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.proxy.ProxyServlet;
+
+public class ConfigurableProxyServlet extends ProxyServlet {
+	private String destinationUri;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// We need to set this here so the async Jetty proxy works as
+		// expected. Supposedly it should work simply with async-supported
+		// on web-fragment.xml and Servlet 3.0, but it does not in our
+		// environment.
+		req.setAttribute("org.apache.catalina.ASYNC_SUPPORTED", true);
+		super.service(req, resp);
+	}
+
+	@Override
+	protected URI rewriteURI(HttpServletRequest request) {
+		String query = request.getQueryString();
+		String uri = destinationUri;
+		if (query != null) {
+			uri += "?" + query;
+		}
+		return URI.create(uri);
+	}
+
+	public void setDestinationUri(String destinationUri) {
+		this.destinationUri = destinationUri;
+	}
+}
